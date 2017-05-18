0400448550d72a01cab26da9e3d87707254081b7
Ariel <ariel.m.little@gmail.com>
2017-05-17 22:40:21 -0400

    moving logic from app.js to handlebars file

--- public/app.js
+++ public/app.js
@@ -2,19 +2,34 @@
  * Created by Ariel - 5.16.2017
  * ================================================== */
 
+var express = require('express'),
+    router = express.Router();
+
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-        // Display the apropos information on the page
-        $("#articles").append(
-            "<p data-id='" + data[i]._id + "'>" +
-                data[i].title + "<br />" +
-                "<img src='" + data[i].link + "'><br />" +
-                data[i].summary +
-            "</p>");
-    }
+    var hbsObject = {
+        foobar:data
+    };
+    console.log('hbsObject', hbsObject);
+    res.render('index', hbsObject);
+
+    // // For each one
+    // for (var i = 0; i < data.length; i++) {
+    //     // Display the apropos information on the page
+    //     $("#articles").append(
+    //         "<p data-id='" + data[i]._id + "'>" +
+    //             data[i].title + "<br />" +
+    //             "<img src='" + data[i].link + "'><br />" +
+    //             data[i].summary +
+    //         "</p>");
+    // }
 });
+
+
+// ==================================================
+// Need to put all the below logic into handlebars
+// ==================================================
+
 
 // user clicks article (<p> tag)
 $(document).on("click", "p", function() {
